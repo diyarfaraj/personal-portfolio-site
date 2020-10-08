15b89f9775890c74cc38efc1542c7dad83f84581
diyarfaraj <diyar.faraj@gmail.com>
2020-10-08 10:39:46 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,123 +1,133 @@
 // Select DOM items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
 
 // set initial state of menu
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-	if (!showMenu) {
-		menuBtn.classList.add('close');
-		menu.classList.add('show');
-		menuNav.classList.add('show');
-		menuBranding.classList.add('show');
-		navItems.forEach((item) => item.classList.add('show'));
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
 
-		showMenu = true;
-	} else {
-		menuBtn.classList.remove('close');
-		menu.classList.remove('show');
-		menuNav.classList.remove('show');
-		menuBranding.classList.remove('show');
-		navItems.forEach((item) => item.classList.remove('show'));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
 
-		showMenu = false;
-	}
+    showMenu = false;
+  }
 }
 
-var Messenger = function(el) {
-	'use strict';
-	var m = this;
+var Messenger = function (el) {
+  "use strict";
+  var m = this;
 
-	m.init = function() {
-		m.codeletters = '&#*+%?£@§$';
-		m.message = 0;
-		m.current_length = 0;
-		m.fadeBuffer = false;
-		m.messages = [ 'Developer', 'E-Commerce' ];
+  m.init = function () {
+    m.codeletters = "&#*+%?£@§$";
+    m.message = 0;
+    m.current_length = 0;
+    m.fadeBuffer = false;
+    m.messages = [
+      "Developer",
+      "E-Commerce",
+      "C#/.NET",
+      "JavaScript/React",
+      "Python",
+    ];
 
-		setTimeout(m.animateIn, 100);
-	};
+    setTimeout(m.animateIn, 100);
+  };
 
-	m.generateRandomString = function(length) {
-		var random_text = '';
-		while (random_text.length < length) {
-			random_text += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
-		}
+  m.generateRandomString = function (length) {
+    var random_text = "";
+    while (random_text.length < length) {
+      random_text += m.codeletters.charAt(
+        Math.floor(Math.random() * m.codeletters.length)
+      );
+    }
 
-		return random_text;
-	};
+    return random_text;
+  };
 
-	m.animateIn = function() {
-		if (m.current_length < m.messages[m.message].length) {
-			m.current_length = m.current_length + 2;
-			if (m.current_length > m.messages[m.message].length) {
-				m.current_length = m.messages[m.message].length;
-			}
+  m.animateIn = function () {
+    if (m.current_length < m.messages[m.message].length) {
+      m.current_length = m.current_length + 2;
+      if (m.current_length > m.messages[m.message].length) {
+        m.current_length = m.messages[m.message].length;
+      }
 
-			var message = m.generateRandomString(m.current_length);
-			$(el).html(message);
+      var message = m.generateRandomString(m.current_length);
+      $(el).html(message);
 
-			setTimeout(m.animateIn, 20);
-		} else {
-			setTimeout(m.animateFadeBuffer, 20);
-		}
-	};
+      setTimeout(m.animateIn, 20);
+    } else {
+      setTimeout(m.animateFadeBuffer, 20);
+    }
+  };
 
-	m.animateFadeBuffer = function() {
-		if (m.fadeBuffer === false) {
-			m.fadeBuffer = [];
-			for (var i = 0; i < m.messages[m.message].length; i++) {
-				m.fadeBuffer.push({
-					c: Math.floor(Math.random() * 12) + 1,
-					l: m.messages[m.message].charAt(i)
-				});
-			}
-		}
+  m.animateFadeBuffer = function () {
+    if (m.fadeBuffer === false) {
+      m.fadeBuffer = [];
+      for (var i = 0; i < m.messages[m.message].length; i++) {
+        m.fadeBuffer.push({
+          c: Math.floor(Math.random() * 12) + 1,
+          l: m.messages[m.message].charAt(i),
+        });
+      }
+    }
 
-		var do_cycles = false;
-		var message = '';
+    var do_cycles = false;
+    var message = "";
 
-		for (var i = 0; i < m.fadeBuffer.length; i++) {
-			var fader = m.fadeBuffer[i];
-			if (fader.c > 0) {
-				do_cycles = true;
-				fader.c--;
-				message += m.codeletters.charAt(Math.floor(Math.random() * m.codeletters.length));
-			} else {
-				message += fader.l;
-			}
-		}
+    for (var i = 0; i < m.fadeBuffer.length; i++) {
+      var fader = m.fadeBuffer[i];
+      if (fader.c > 0) {
+        do_cycles = true;
+        fader.c--;
+        message += m.codeletters.charAt(
+          Math.floor(Math.random() * m.codeletters.length)
+        );
+      } else {
+        message += fader.l;
+      }
+    }
 
-		$(el).html(message);
+    $(el).html(message);
 
-		if (do_cycles === true) {
-			setTimeout(m.animateFadeBuffer, 50);
-		} else {
-			setTimeout(m.cycleText, 3000);
-		}
-	};
+    if (do_cycles === true) {
+      setTimeout(m.animateFadeBuffer, 50);
+    } else {
+      setTimeout(m.cycleText, 3000);
+    }
+  };
 
-	m.cycleText = function() {
-		m.message = m.message + 1;
-		if (m.message >= m.messages.length) {
-			m.message = 0;
-		}
+  m.cycleText = function () {
+    m.message = m.message + 1;
+    if (m.message >= m.messages.length) {
+      m.message = 0;
+    }
 
-		m.current_length = 0;
-		m.fadeBuffer = false;
-		$(el).html('');
+    m.current_length = 0;
+    m.fadeBuffer = false;
+    $(el).html("");
 
-		setTimeout(m.animateIn, 200);
-	};
+    setTimeout(m.animateIn, 200);
+  };
 
-	m.init();
+  m.init();
 };
 
 console.clear();
-var messenger = new Messenger($('#messenger'));
+var messenger = new Messenger($("#messenger"));
